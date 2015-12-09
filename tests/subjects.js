fe9e75fabd1e33431864a2b3bf70474e0b651eb6
Garry M. Perez <perez.garrym@gmail.com>
2015-12-10 03:28:13 +0800

    subjects

--- tests/subjects.js
+++ tests/subjects.js
@@ -9,82 +9,178 @@ define(['model'],function($model){
 					{
 					  "id": "G1ENGLI",
 					  "name": "English",
-					  "description": "English 1"
+					  "description": "English 1",
+					  "alias": "Eng 1",
+					  "unit": 1,
+					  "YearLevel":{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G2ENGLI",
 					  "name": "English",
-					  "description": "English 2"
+					  "description": "English 2",
+					  "alias": "Eng 2",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G3ENGLI",
 					  "name": "English",
-					  "description": "English 3"
+					  "description": "English 3",
+					  "alias": "Eng 3",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G3FILIP",
 					  "name": "Filipino",
-					  "description": "Filipino 3"
+					  "description": "Filipino 3",
+					  "alias": "Fil 3",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G1FILIP",
 					  "name": "Filipino",
-					  "description": "Filipino 1"
+					  "description": "Filipino 1",
+					  "alias": "Fil 1",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G2FILIP",
 					  "name": "Filipino",
-					  "description": "Filipino 2"
+					  "description": "Filipino 2",
+					  "alias": "Fil 2",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G3MATHCS",
 					  "name": "Mathematics",
-					  "description": "Mathematics 3"
+					  "description": "Mathematics 3",
+					  "alias": "Matn 3",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G3SCINCE",
 					  "name": "Science",
-					  "description": "Science 3"
+					  "description": "Science 3",
+					  "alias": "Sci 1",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G1SCIENCE",
 					  "name": "Science",
-					  "description": "Science 1"
+					  "description": "Science 1",
+					  "alias": "Sci 1",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G2SCIENCE",
 					  "name": "Science",
-					  "description": "Science 2"
+					  "description": "Science 2",
+					  "alias": "Sci 2",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G3COMPTR",
 					  "name": "Computer",
-					  "description": "Computer 3"
+					  "description": "Computer 3",
+					  "alias": "Comp 3",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G1COMPTR",
 					  "name": "Computer",
-					  "description": "Computer 1"
+					  "description": "Computer 1",
+					  "alias": "Comp 1",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G2COMPTR",
 					  "name": "Computer",
-					  "description": "Computer 2"
+					  "description": "Computer 2",
+					  "alias": "Comp 2",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G2ARALP",
 					  "name": "Araling Panlipunan",
-					  "description": "Araling Panlipunan 2"
+					  "description": "Araling Panlipunan 2",
+					  "alias": "AP 2",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G1ARALP",
 					  "name": "Araling Panlipunan",
-					  "description": "Araling Panlipunan 1"
+					  "description": "Araling Panlipunan 1",
+					  "alias": "AP 1",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 					{
 					  "id": "G3ARALP",
 					  "name": "Araling Panlipunan",
-					  "description": "Araling Panlipunan 3"
+					  "description": "Araling Panlipunan 3",
+					  "alias": "AP 3",
+					  "unit": 1,
+					  YearLevel:{
+						  "id":"G1",
+						  "name":"Grade 1"
+					  }
 					},
 				]
 			}
