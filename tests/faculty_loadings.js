ca54d0346c7d12580e287e1ffda0d47856a469d6
Garry M. Perez <perez.garrym@gmail.com>
2015-12-10 07:07:40 +0800

    faculty loadings

---
+++ tests/faculty_loadings.js
@@ -0,0 +1,96 @@
+"use strict";
+define(['model'],function($model){
+	return new $model(
+			{
+				meta:{
+					title: 'Faculty Loadings',
+				},
+				data:[
+					{
+						"id": 1,
+						"subject":{
+						  "id":"ENGLSH1",
+						  "name":" English",
+						  "description":"English 1"
+						},
+						"sy": 2015,
+						"educ_level":{
+						  "name":"High School",
+						  "id":"HS"
+						},
+						"section":{
+							"id":1001,
+							"name": "PILOT"
+						},
+						"year_level":{
+							"id":"G7",
+							"name":"Grade 7"
+						}
+					},
+					{
+						"id": 1,
+						"subject":{
+						  "id":"FILIP1",
+						  "name":" Filipino",
+						  "description":"Filipino 1"
+						},
+						"sy": 2015,
+						"educ_level":{
+						  "name":"High School",
+						  "id":"HS"
+						},
+						"section":{
+							"id":1001,
+							"name": "PILOT"
+						},
+						"year_level":{
+							"id":"G7",
+							"name":"Grade 7"
+						}
+					},
+					{
+						"id": 1,
+						"subject":{
+						  "id":"SCIENCE1",
+						  "name":" Science",
+						  "description":"Science 1"
+						},
+						"sy": 2015,
+						"educ_level":{
+						  "name":"High School",
+						  "id":"HS"
+						},
+						"section":{
+							"id":1001,
+							"name": "PILOT"
+						},
+						"year_level":{
+							"id":"G7",
+							"name":"Grade 7"
+						}
+					},
+					{
+						"id": 1,
+						"subject":{
+						  "id":"MATH1",
+						  "name":"Mathematics",
+						  "description":"Mathematics 1"
+						},
+						"sy": 2015,
+						"educ_level":{
+						  "name":"High School",
+						  "id":"HS"
+						},
+						"section":{
+							"id":1001,
+							"name": "PILOT"
+						},
+						"year_level":{
+							"id":"G7",
+							"name":"Grade 7"
+						}
+					},
+				]
+			}
+		);
+});
